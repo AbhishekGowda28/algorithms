886020d5110a04b88555538e912a21506b1d4c79
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-01-12 21:38:05 +0530

    Optimized finding Prime Numbers

--- project-euler/__/helper/primeNumber.py
+++ project-euler/__/helper/primeNumber.py
@@ -6,12 +6,18 @@ def primeNumbers(start, last):
         print("Prime Numbers cannot be found")
         return []
     rangeList = defineRange(start, last+1)
-    primes = []
-
-    for count in range(2, math.ceil(math.sqrt(last))):
-        for block in rangeList:
+    primes = [2]
+    mark = 0
+    count = rangeList[mark][0]
+    while count < math.ceil(math.sqrt(last+1)):
+        for block in rangeList[mark+1:]:
             if block[0] % count == 0 and block[0] != count and block[1] == True:
                 block[1] = False
+        mark += 1
+        while rangeList[mark][1] == False:
+            mark += 1
+        count = rangeList[mark][0]
+
     for block in rangeList:
         if block[1] == True:
             primes.append(block[0])
@@ -20,6 +26,8 @@ def primeNumbers(start, last):
 
 
 def isPrime(number):
+    if number != 2 and number % 2 == 0:
+        return False
     primes = primeNumbers(2, number)
     return primes.__contains__(number)
 
@@ -29,8 +37,10 @@ def defineRange(start, end):
     if start > 1 and end > 1:
         if start < end:
             for number in range(start, end):
-                rangeList.append([number, True])
+                if number % 2 != 0:
+                    rangeList.append([number, True])
         else:
             for number in range(end, start):
-                rangeList.append([number, True])
+                if number % 2 != 0:
+                    rangeList.append([number, True])
     return rangeList
--- project-euler/__/problem-10-prime-sum.py
+++ project-euler/__/problem-10-prime-sum.py
@@ -1,7 +1,7 @@
 from helper import primeNumber
 
 def problem10():
-    primes = primeNumber.primeNumbers(2,10)
+    primes = primeNumber.primeNumbers(2,2000000)
     primeSum = 0
     for prime in primes:
         primeSum+=prime
