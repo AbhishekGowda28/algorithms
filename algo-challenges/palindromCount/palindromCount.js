53ac1005b811ef984e3dfd46f83597ed9211ae25
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-02-23 22:31:11 +0530

    Update palindromCount.js

--- algo-challenges/palindromCount/palindromCount.js
+++ algo-challenges/palindromCount/palindromCount.js
@@ -1,7 +1,21 @@
 function palindromCount(str) {
     let largestPalindrom = 0;
     let counter = 1;
-let endCounter = str.length - 1;
+    let endCounter = str.length;
+    while(counter < endCounter) {
+     console.log(str[counter];
+     let distance = 1;
+     let previous = str[counter - distance];
+     let next = str[counter + distance];
+     let intermediatePalindromCount = 1;
+     while(previous === next){
+      distance++;
+      intermediatePalindromCount+=2;
+      previous = str[counter - distance];
+      next = str[counter + distance];
+     }
+     counter++;
+    }
     return str;
 }
 
