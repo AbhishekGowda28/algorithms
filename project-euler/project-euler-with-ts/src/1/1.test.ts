c6d4b7f7066b114ba26c68226ae1b4246048b710
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-02-25 18:35:30 +0530

    Update 1.test.ts

--- project-euler/project-euler-with-ts/src/1/1.test.ts
+++ project-euler/project-euler-with-ts/src/1/1.test.ts
@@ -2,5 +2,6 @@ import { problem1 } from "./1";
 
 test("Testing problem 1", () => {
     const result = problem1();
-    expect(result).toBe(14);
+    console.log(result);
+    expect(result).toBe(140);
 });
