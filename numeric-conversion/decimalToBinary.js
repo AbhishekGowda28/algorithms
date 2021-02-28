e3b7a57ecdd8f54439b23a8cba4b25ffb753654a
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-02-28 20:08:14 +0530

    Solved decimal to binary conversion

--- numeric-conversion/decimalToBinary.js
+++ numeric-conversion/decimalToBinary.js
@@ -1,13 +1,20 @@
+// @ts-check
+
+/**
+ * @param {number} decimal
+ * @param {number} base
+ */
 function decimalToBinary(decimal, base) {
     const binaryArray = [];
     while (decimal > 1) {
         const reminder = decimal % base;
-        decimal = Number(decimal / base).toFixed(0);
+        decimal = Math.floor(decimal / base);
         binaryArray.unshift(reminder);
     }
     binaryArray.unshift(decimal);
     return binaryArray;
+
 }
 
-console.log(decimalToBinary(10, 2));
-console.log(decimalToBinary(12, 2));
+console.log(10, decimalToBinary(10, 2));
+console.log(12, decimalToBinary(12, 2));
